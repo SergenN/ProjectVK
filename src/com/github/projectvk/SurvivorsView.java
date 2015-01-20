bbf9578d0d68ea935f7b301874a7d43cc92f28e8
Thijsku <thijs.kuilman@gmail.com>
2015-01-20 20:30:28 +0100

    img folder

--- src/com/github/projectvk/SurvivorsView.java
+++ src/com/github/projectvk/SurvivorsView.java
@@ -12,25 +12,51 @@ import com.xeiam.xchart.StyleManager.LegendPosition;
 import com.xeiam.xchart.SwingWrapper;
 import com.xeiam.xchart.demo.charts.ExampleChart;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class SurvivorsView implements ExampleChart {
 
+
+    private List<Double> rabbitsList = new ArrayList<Double>();
+
     public SurvivorsView(){
-        System.out.println("test");
+        allData();
         Chart chart = getChart();
         new SwingWrapper(chart).displayChart();
     }
 
+    public void allData(){
+        for (int i = 0; i < 20; i++) {
+            rabbitsList.add(32.0);
+            rabbitsList.add(22.0);
+            rabbitsList.add(42.0);
+            rabbitsList.add(32.0);
+            rabbitsList.add(23.0);
+        }
+    }
+
     public Chart getChart() {
-        double []  rabbits = { 7, 10, 8, 15, 12 };
+        double[] turns = new double[rabbitsList.size()];
+        double[] rabbits = new double[rabbitsList.size()];
         double []  foxes = { 8, 4, 6, 6, 8 };
         double []  hunters = { 0, 5, 2, 3, 5 };
 
+        // Sum of turns
+        for (int i = 0; i < turns.length; i++){
+            turns[i] = i;
+        }
+
+        // Fill Rabbit Data
+        for (int i = 0; i < rabbits.length; i++){
+            rabbits[i] = rabbitsList.get(i);
+        }
+
         // Create Chart
         Chart chart = new ChartBuilder().chartType(ChartType.Area).width(600).height(400).title(getClass().getSimpleName()).xAxisTitle("Stap").yAxisTitle("Aantallen").build();
-        chart.addSeries("Vossen", new double[] { 0, 3, 5, 7, 9 }, foxes);
-        chart.addSeries("Konijnen", new double[] { 0, 3, 5, 7, 9 }, rabbits);
-        chart.addSeries("Jagers", new double[] { 0, 1, 3, 8, 9 }, hunters);
+        chart.addSeries("Konijnen", turns, rabbits);
 
         // Customize Chart
         chart.getStyleManager().setLegendPosition(LegendPosition.InsideNW);
