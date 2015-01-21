cab28203b7f7c05f4e8bd24c60c37cbd6ad5407c
Sergen Nurel <sergen_nurel@hotmail.com>
2015-01-21 11:50:55 +0100

    Thread runner toegevoegd; moet nog geimplementeerd en getest worden

--- src/com/github/projectvk/runner/ThreadRunner.java
+++ src/com/github/projectvk/runner/ThreadRunner.java
@@ -1,5 +1,7 @@
 package com.github.projectvk.runner;
 
+import com.github.projectvk.model.Simulator;
+
 /**
  * Created by Sergen on 21-1-2015.
  */
@@ -7,19 +9,30 @@ public class ThreadRunner implements Runnable{
 
     private int steps;
     private boolean inf;
+    private Simulator sim;
+    private final long delay = 500;
 
-    public ThreadRunner(){
+    public ThreadRunner(Simulator sim){
         steps = 0;
         inf = false;
+        this.sim = sim;
     }
 
     public void runSimulate(int steps){
         this.steps += steps;
+        if(this.steps > 0) {
+            if (!Thread.currentThread().isAlive()) {
+                new Thread(this).run();
+            }
+        }
     }
 
     public void runSimulate(boolean infinite){
+        this.inf = infinite;
         if(infinite){
-
+            if(!Thread.currentThread().isAlive()){
+                new Thread(this).run();
+            }
         }
     }
 
@@ -28,9 +41,23 @@ public class ThreadRunner implements Runnable{
         steps = 0;
     }
 
+    private void decrementSteps(){
+        if(steps > 0){
+            steps--;
+        }
+    }
+
 
     @Override
     public void run() {
-
+        if(steps > 0 || inf){
+            sim.simulateOneStep();
+            decrementSteps();
+        }
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
